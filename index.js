850600fd3e64a05a30d6b4d31ef8116f7ba69218
anurag1109 <bittu.anurag123@gmail.com>
2022-11-02 03:58:58 +0530

    code fixed

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 
-let num=20;
-count=0;
+let num = 20;
+count = 0;
 
-for(let i=1;i<=num;i++){
-  for(let j=1;j<=i/2;j++){
-    if(i%j==0)
+for (let i = 1; i <= num; i++) {
+  for (let j = 1; j <= i / 2; j++) {
+    if (i % j == 0)
       count++;
   }
-  if(count==1)
+  if (count == 1)
     console.log(i);
+  count = 0;
 
-  }
-  
+}
 
-  
+
+
